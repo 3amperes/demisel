0a87e0842d371d0bfe0ce5ad12623af49b27a8e0
wabdsgn <wab2com@gmail.com>
2019-03-24 18:37:09 +0100

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,57 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // your wordpress source
+        baseUrl: `demiselbijoux.com`,
+        protocol: `https`,
+        // is it hosted on wordpress.com, or self-hosted?
+        hostingWPCOM: false,
+        // does your site use the Advanced Custom Fields Plugin?
+        useACF: false,
+      },
+    },
+    {
+      resolve: "@massivdash/gatsby-source-woocommerce",
+      options: {
+        // Base URL of Wordpress site
+        api: "demiselbijoux.com",
+
+        // This counts controls the API get with ?per_page=
+        // default: 10
+        itemCount: 20,
+
+        // set to true to see fetch output in console, during build
+        // default: false
+        verbose: true,
+
+        // true if using https. false if nah.
+        https: false,
+        api_keys: {
+          consumer_key: "ck_6d27a12f193461bb5c7ba05d5d4402203c1cf99f",
+          consumer_secret: "cs_5396e9ee07395c40acc760855533ae93087889b6",
+        },
+        // Array of strings with fields you'd like to create nodes for...
+        fields: ["products"],
+      },
+    },
+    // {
+    //   resolve: "gatsby-source-woocommerce",
+    //   options: {
+    //     // Base URL of Wordpress site
+    //     api: "demiselbijoux.com",
+    //     // true if using https. false if nah.
+    //     https: true,
+    //     api_keys: {
+    //       consumer_key: "ck_6d27a12f193461bb5c7ba05d5d4402203c1cf99f",
+    //       consumer_secret: "cs_5396e9ee07395c40acc760855533ae93087889b6",
+    //     },
+    //     // Array of strings with fields you'd like to create nodes for...
+    //     fields: ["products"],
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
