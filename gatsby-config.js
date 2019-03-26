3c5e35a04b008a85df3ab5548ae5ffd2d2cf75b8
wabdsgn <wab2com@gmail.com>
2019-03-26 23:26:16 +0100

    add product list on page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `demiselbijoux lab`,
+    description: `desc`,
+    author: `@wabdsgn`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -37,4 +37,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
