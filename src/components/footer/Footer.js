d519a7add6d6da57641d843aa847f77cd7502166
wabdsgn <wab2com@gmail.com>
2019-10-03 17:53:30 +0200

    add items to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StaticQuery, Link, graphql } from 'gatsby';
+import { Text } from 'rebass/styled-components';
 import { container } from '@utils/mixins';
+import { colors } from '@theme';
 
 const Wrapper = styled.footer`
   display: grid;
@@ -10,12 +12,24 @@ const Wrapper = styled.footer`
   grid-row-gap: 100px;
   padding: 150px 0;
   ${container}
+
+  a {
+    color: ${colors.warmGrey};
+    text-decoration: none;
+    font-size: 14px;
+  }
 `;
+
+const Title = ({ children }) => (
+  <Text fontFamily="orpheuspro" fontWeight="400" mb="1.5rem">
+    {children}
+  </Text>
+);
 
 const Categories = ({ items }) =>
   items.length > 0 ? (
     <div>
-      <strong>Les catégories:</strong>
+      <Title>Collections</Title>
       <ul>
         {items.map(node => {
           return (
@@ -50,13 +64,55 @@ const Footer = () => (
         <Wrapper>
           <Categories items={data.allSanityCategory.nodes}></Categories>
           <div>
-            <strong>Entreprise</strong>
+            <Title>Entreprise</Title>
+            <ul>
+              <li>
+                <Link to="/brand">La marque</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contactez-nous</Link>
+              </li>
+              <li>
+                <Link to="/rgpd">Politique de confidentialité</Link>
+              </li>
+              <li>
+                <Link to="/cgu">CGV - CGU</Link>
+              </li>
+              <li>
+                <Link to="/shipping">Livraison et retour</Link>
+              </li>
+              <li>
+                <Link to="/legal">Mentions légales</Link>
+              </li>
+            </ul>
           </div>
           <div>
-            <strong>Partenaires</strong>
+            <Title>Partenaires</Title>
+            <ul>
+              <li>
+                <Link to="/selling-points">Nos points de vente</Link>
+              </li>
+              <li>
+                <Link to="/dealer-acces">Accès revendeur</Link>
+              </li>
+              <li>
+                <Link to="/press">Presse</Link>
+              </li>
+              <li>
+                <Link to="/lookbook">Lookbook 19/20</Link>
+              </li>
+            </ul>
           </div>
           <div>
-            <strong>Suivez-nous</strong>
+            <Title>Suivez-nous</Title>
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/demiselbijoux">Instagram</a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/demiselbijoux/">Facebook</a>
+              </li>
+            </ul>
           </div>
           <div>fabrication française</div>
           <div>
