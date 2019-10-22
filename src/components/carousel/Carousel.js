37e73749109b717454afc22a50cfbe67a2aae302
wabdsgn <wab2com@gmail.com>
2019-10-22 14:47:32 +0200

    add multiple item with transitions

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Flex, Box, Heading, Text } from 'rebass/styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+import { Box } from 'rebass/styled-components';
 import Item from './Item';
 
-const Wrapper = styled(Flex)`
+const Wrapper = styled(Box)`
   width: 100%;
-  height: 600px;
+  min-height: 400px;
+  position: relative;
+  transition: all 250ms ease-in-out;
 `;
-export default props => <Item />;
+export default props => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          image1: file(relativePath: { eq: "slider.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1200) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          image2: file(relativePath: { eq: "slider2.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1200) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <>
+            <Wrapper>
+              <Item
+                fluid={data.image1.childImageSharp.fluid}
+                isCurrent={currentIndex === 0}
+              />
+              <Item
+                fluid={data.image2.childImageSharp.fluid}
+                isCurrent={currentIndex === 1}
+              />
+            </Wrapper>
+            <button onClick={() => setCurrentIndex(currentIndex === 0 ? 1 : 0)}>
+              next
+            </button>
+          </>
+        );
+      }}
+    />
+  );
+};
--- src/components/carousel/Item.js
+++ src/components/carousel/Item.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 import styled from 'styled-components';
 import { Box, Flex, Heading, Text } from 'rebass/styled-components';
@@ -7,8 +6,13 @@ import { colors } from '@theme';
 import { container } from '@utils/mixins';
 
 const Wrapper = styled(Box)`
-  position: relative;
+  width: 100%;
   height: 100%;
+  top: 0;
+  left: 0;
+  opacity: ${props => (props.isCurrent ? 1 : 0)};
+  position: ${props => (props.isCurrent ? 'relative' : 'absolute')};
+  transition: all 250ms ease-in-out;
 `;
 const Inner = styled(Flex)`
   position: absolute;
@@ -47,40 +51,25 @@ const Button = styled.button`
   }
 `;
 
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        item: file(relativePath: { eq: "slider.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 1200) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      return (
-        <Wrapper>
-          <Image height={400} fluid={data.item.childImageSharp.fluid} />
-          <Inner>
-            <Grid>
-              <div>
-                <Heading fontSize={[32, 48]} lineHeight={1.2} as="h2" mb="1rem">
-                  Nouvelle collection en cuir végétal
-                </Heading>
-                <Text fontSize={14} lineHeight={1.8} mb="60px">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-                  libero sequi voluptates corporis vitae eligendi rerum
-                  reiciendis, provident error totam.
-                </Text>
-                <Button>Découvrir</Button>
-              </div>
-            </Grid>
-          </Inner>
-        </Wrapper>
-      );
-    }}
-  />
-);
+export default ({ fluid, ...rest }) => {
+  return (
+    <Wrapper {...rest}>
+      <Image height={400} fluid={fluid} />
+      <Inner>
+        <Grid>
+          <div>
+            <Heading fontSize={[32, 48]} lineHeight={1.2} as="h2" mb="1rem">
+              Nouvelle collection en cuir végétal
+            </Heading>
+            <Text fontSize={14} lineHeight={1.8} mb="60px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+              libero sequi voluptates corporis vitae eligendi rerum reiciendis,
+              provident error totam.
+            </Text>
+            <Button>Découvrir</Button>
+          </div>
+        </Grid>
+      </Inner>
+    </Wrapper>
+  );
+};
