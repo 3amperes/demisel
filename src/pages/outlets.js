6b84e00754dc7229d0cb67c8949dd668d1095e58
wabdsgn <wab2com@gmail.com>
2019-11-15 00:07:23 +0100

    query

--- src/pages/outlets.js
+++ src/pages/outlets.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import Image from 'gatsby-image';
 import styled from 'styled-components';
 import { Heading, Text, Box } from 'rebass/styled-components';
 import { MainLayout } from '@layouts';
@@ -14,23 +14,6 @@ const offset = 160;
 const Header = styled.header`
   ${coloredSection('738px', '-15deg')};
 `;
-
-const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "outlets.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 800) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
-);
 
 const Outlet = ({ name, city, url }) => (
   <Box py=".5rem">
@@ -56,61 +39,69 @@ const Country = ({ children, name, outlets, ...rest }) => (
   </Box>
 );
 
-const outlets = [
-  {
-    name: 'Yuman Village',
-    city: 'Bruxelles',
-    url: 'http://demiselbijoux.com',
-  },
-  {
-    name: 'Yuman Village',
-    city: 'Bruxelles',
-    url: 'http://demiselbijoux.com',
-  },
-  {
-    name: 'Yuman Village',
-    city: 'Bruxelles',
-    url: 'http://demiselbijoux.com',
-  },
-  {
-    name: 'Yuman Village',
-    city: 'Bruxelles',
-    url: 'http://demiselbijoux.com',
-  },
-  {
-    name: 'Yuman Village',
-    city: 'Bruxelles',
-    url: 'http://demiselbijoux.com',
-  },
-];
-
-const Outlets = () => (
-  <MainLayout>
-    <SEO title="Nos points de vente" />
-    <Header>
-      <Heading as="h1" fontSize={[32, 56]} lineHeight="1.2" mb="0.5em">
-        Nos points de vente
-      </Heading>
-      <Text
-        maxWidth="338px"
-        mb={`${offset}px`}
-        color="greyishBrown"
-        fontSize={14}
-        lineHeight="24px"
-        textAlign="center"
-      >
-        Nous sommes là, mais nous sommes aussi ici et puis là-bas… en fait nos
-        bijoux sont vendus un peu partout en France et même en Europe !
-      </Text>
-    </Header>
-    <Box maxWidth="700px" mx="auto" mt={`-${offset}px`}>
-      <Image />
-    </Box>
-    <Box maxWidth="700px" mx="auto" mt={`${offset}px`} textAlign="center">
-      <Country name="Belgique" outlets={outlets} />
-    </Box>
-    <Newsletter />
-  </MainLayout>
+const Countries = ({ items }) => (
+  <Box maxWidth="700px" mx="auto" my={`${offset}px`} textAlign="center">
+    {items.map(({ node: { name, outlets } }) => (
+      <Country name={name} outlets={outlets} />
+    ))}
+  </Box>
 );
 
-export default Outlets;
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        introImage: file(relativePath: { eq: "outlets.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        countries: allSanityCountry(sort: { fields: name }) {
+          edges {
+            node {
+              id
+              name
+              outlets {
+                _key
+                name
+                city
+                url
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      return (
+        <MainLayout>
+          <SEO title="Nos points de vente" />
+          <Header>
+            <Heading as="h1" fontSize={[32, 56]} lineHeight="1.2" mb="0.5em">
+              Nos points de vente
+            </Heading>
+            <Text
+              maxWidth="338px"
+              mb={`${offset}px`}
+              color="greyishBrown"
+              fontSize={14}
+              lineHeight="24px"
+              textAlign="center"
+            >
+              Nous sommes là, mais nous sommes aussi ici et puis là-bas… en fait
+              nos bijoux sont vendus un peu partout en France et même en Europe
+              !
+            </Text>
+          </Header>
+          <Box maxWidth="700px" mx="auto" mt={`-${offset}px`}>
+            <Image fluid={data.introImage.childImageSharp.fluid} />
+          </Box>
+          <Countries items={data.countries.edges} />
+          <Newsletter />
+        </MainLayout>
+      );
+    }}
+  />
+);
