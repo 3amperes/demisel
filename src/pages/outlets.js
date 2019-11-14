48dda120d38d25671ec278212f01583775a5bd32
wabdsgn <wab2com@gmail.com>
2019-11-14 23:42:10 +0100

    clean

--- src/pages/outlets.js
+++ src/pages/outlets.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import styled from 'styled-components';
 import { Heading, Text, Box } from 'rebass/styled-components';
 import { MainLayout } from '@layouts';
-import SEO from '@components/seo';
 import { coloredSection } from '@utils/mixins';
-import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import { colors } from '@theme';
+import SEO from '@components/seo';
+import Newsletter from '@components/newsletter';
 
 const offset = 160;
 
@@ -90,7 +91,14 @@ const Outlets = () => (
       <Heading as="h1" fontSize={[32, 56]} lineHeight="1.2" mb="0.5em">
         Nos points de vente
       </Heading>
-      <Text maxWidth="338px" mb={`${offset}px`}>
+      <Text
+        maxWidth="338px"
+        mb={`${offset}px`}
+        color="greyishBrown"
+        fontSize={14}
+        lineHeight="24px"
+        textAlign="center"
+      >
         Nous sommes là, mais nous sommes aussi ici et puis là-bas… en fait nos
         bijoux sont vendus un peu partout en France et même en Europe !
       </Text>
@@ -101,6 +109,7 @@ const Outlets = () => (
     <Box maxWidth="700px" mx="auto" mt={`${offset}px`} textAlign="center">
       <Country name="Belgique" outlets={outlets} />
     </Box>
+    <Newsletter />
   </MainLayout>
 );
 
