afe1c889af234bd6113887a6665db962e81b5ba5
wabdsgn <wab2com@gmail.com>
2019-11-18 22:29:47 +0100

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,13 +61,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `demisel`,
-        short_name: `starter`,
+        name: `Demisel Bijoux`,
+        short_name: `demisel`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#e33450`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
