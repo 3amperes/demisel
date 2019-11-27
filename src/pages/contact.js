a646718bec365138026d3d44fc4701b4fe1ccd83
wabdsgn <wab2com@gmail.com>
2019-11-27 01:09:02 +0100

    add state thanks page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import { navigate } from 'gatsby-link';
 import styled from 'styled-components';
 import { Heading, Text, Box, Flex } from 'rebass/styled-components';
 import { MainLayout } from '@layouts';
@@ -9,6 +10,12 @@ import SEO from '@components/seo';
 import Newsletter from '@components/newsletter';
 
 const offset = 160;
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
 
 const Header = styled.header`
   ${coloredSection('738px', '-15deg')};
@@ -68,111 +75,157 @@ const Field = ({ label, id = '', children }) => (
   </Box>
 );
 
-export default () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        introImage: file(relativePath: { eq: "outlets.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 800) {
-              ...GatsbyImageSharpFluid
+export default () => {
+  const [state, setState] = useState({});
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => alert(error));
+  };
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          introImage: file(relativePath: { eq: "outlets.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+              }
             }
           }
-        }
-        countries: allSanityCountry(sort: { fields: name }) {
-          edges {
-            node {
-              id
-              name
-              outlets {
-                _key
+          countries: allSanityCountry(sort: { fields: name }) {
+            edges {
+              node {
+                id
                 name
-                city
-                url
+                outlets {
+                  _key
+                  name
+                  city
+                  url
+                }
               }
             }
           }
         }
-      }
-    `}
-    render={data => {
-      return (
-        <MainLayout bg="whiteTwo">
-          <SEO title="Contact" />
-          <Header>
-            <Heading as="h1" fontSize={[32, 56]} lineHeight="1.2" mb="0.5em">
-              Contactez-nous
-            </Heading>
-            <Text
-              maxWidth="338px"
-              mb={`${offset}px`}
-              color="greyishBrown"
-              fontSize={14}
-              lineHeight="24px"
-              textAlign="center"
-            >
-              On adore avoir de vos nouvelles ! Alors que ce soir pour une
-              question sur un produit, sur une disponibilité, sur nos
-              collections, pour parler up cycling ou zéro déchet ou encore pour
-              devenir revendeur de nos bijoux, envoyez nous votre message !
-            </Text>
-          </Header>
-          <Flex>
-            <Box
-              p={[20, 120]}
-              bg="white"
-              mt={`-${offset}px`}
-              mb={`${offset}px`}
-              ml="auto"
-              mr="auto"
-              width={[1, '700px']}
-            >
-              <Heading
-                textAlign="center"
-                as="h2"
-                fontSize={24}
-                lineHeight="1.2"
-                mb="0.5em"
-              >
-                Votre message
+      `}
+      render={data => {
+        return (
+          <MainLayout bg="whiteTwo">
+            <SEO title="Contact" />
+            <Header>
+              <Heading as="h1" fontSize={[32, 56]} lineHeight="1.2" mb="0.5em">
+                Contactez-nous
               </Heading>
-              <Form
-                method="post"
-                name="contact"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
+              <Text
+                maxWidth="338px"
+                mb={`${offset}px`}
+                color="greyishBrown"
+                fontSize={14}
+                lineHeight="24px"
+                textAlign="center"
               >
-                <input type="hidden" name="form-name" value="contact" />
-                <input type="hidden" name="bot-field" />
-                <Field label="Votre demande" id="demand">
-                  <input type="text" name="demand" required />
-                </Field>
-                <Field label="Votre nom complet">
-                  <input type="text" name="name" required />
-                </Field>
-                <Field label="Votre email">
-                  <input type="email" name="email" required />
-                </Field>
-                <Field label="Votre entreprise (facultatif)">
-                  <input type="text" name="company" />
-                </Field>
-                <Field label="Votre demande">
-                  <textarea
-                    name="message"
-                    cols="30"
-                    rows="10"
-                    required
-                  ></textarea>
-                </Field>
-                <Box mt={['1rem', '4rem']}>
-                  <button type="submit">Envoyer la missive</button>
-                </Box>
-              </Form>
-            </Box>
-          </Flex>
-          <Newsletter />
-        </MainLayout>
-      );
-    }}
-  />
-);
+                On adore avoir de vos nouvelles ! Alors que ce soir pour une
+                question sur un produit, sur une disponibilité, sur nos
+                collections, pour parler up cycling ou zéro déchet ou encore
+                pour devenir revendeur de nos bijoux, envoyez nous votre message
+                !
+              </Text>
+            </Header>
+            <Flex>
+              <Box
+                p={[20, 120]}
+                bg="white"
+                mt={`-${offset}px`}
+                mb={`${offset}px`}
+                ml="auto"
+                mr="auto"
+                width={[1, '700px']}
+              >
+                <Heading
+                  textAlign="center"
+                  as="h2"
+                  fontSize={24}
+                  lineHeight="1.2"
+                  mb="0.5em"
+                >
+                  Votre message
+                </Heading>
+                <Form
+                  name="contact"
+                  method="post"
+                  action="/thanks/"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                  onSubmit={handleSubmit}
+                >
+                  <input type="hidden" name="form-name" value="contact" />
+                  <input
+                    type="hidden"
+                    name="bot-field"
+                    onChange={handleChange}
+                  />
+                  <Field label="Votre demande" id="demand">
+                    <input
+                      type="text"
+                      name="demand"
+                      required
+                      onChange={handleChange}
+                    />
+                  </Field>
+                  <Field label="Votre nom complet">
+                    <input
+                      type="text"
+                      name="name"
+                      required
+                      onChange={handleChange}
+                    />
+                  </Field>
+                  <Field label="Votre email">
+                    <input
+                      type="email"
+                      name="email"
+                      required
+                      onChange={handleChange}
+                    />
+                  </Field>
+                  <Field label="Votre entreprise (facultatif)">
+                    <input type="text" name="company" onChange={handleChange} />
+                  </Field>
+                  <Field label="Votre demande">
+                    <textarea
+                      name="message"
+                      cols="30"
+                      rows="10"
+                      required
+                      onChange={handleChange}
+                    ></textarea>
+                  </Field>
+                  <Box mt={['1rem', '4rem']}>
+                    <button type="submit">Envoyer la missive</button>
+                  </Box>
+                </Form>
+              </Box>
+            </Flex>
+            <Newsletter />
+          </MainLayout>
+        );
+      }}
+    />
+  );
+};
