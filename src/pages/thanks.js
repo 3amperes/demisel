1810660809e23ade060c04e0385abb5c14780ad4
wabdsgn <wab2com@gmail.com>
2019-12-05 16:27:26 +0100

    :lipstick: thanx page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import { MainLayout } from '@layouts';
 import SEO from '@components/seo';
+import { container } from '@utils/mixins';
+
+const Wrapper = styled.div`
+  ${container}
+`;
 
 const NotFoundPage = () => (
   <MainLayout>
     <SEO title="Merci" />
-    <h1>Votre message</h1>
-    <p>Votre message a bien été envoyé, à très vite !</p>
+
+    <Wrapper>
+      <h1>Merci !</h1>
+      <p>Votre message a bien été envoyé, à très vite !</p>
+    </Wrapper>
   </MainLayout>
 );
 
