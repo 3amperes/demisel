88fd823f827abe5a57057dd6cc02a191936ad8ee
wabdsgn <wab2com@gmail.com>
2020-01-08 12:03:26 +0100

    :bug: fix static discount price

--- src/components/product/AddButton.js
+++ src/components/product/AddButton.js
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { StaticQuery, graphql } from 'gatsby';
 import { Box } from 'rebass/styled-components';
 import { shade } from 'polished';
 import { hasPrice, getPrice } from '@utils';
 import { colors } from '@theme';
 import { BasketIcon } from '@components/header';
-import { GlobalContext } from '@components/globalStore';
 import { useBreakpoint } from '@utils/hooks';
 
 const Button = styled.button`
@@ -36,45 +36,61 @@ const Button = styled.button`
 `;
 
 const AddButton = ({ product, rounded }) => {
-  const {
-    state: { discountsAreEnabled },
-  } = useContext(GlobalContext);
   const isDesktop = useBreakpoint('desktop');
   const isDisabled = !hasPrice(product);
   const productName = `${product.category.shortName} ${product.model &&
     product.model.title} ${product.title}`;
-  const price =
-    (discountsAreEnabled && getPrice(product, 'discountPrice')) ||
-    getPrice(product, 'salePrice');
 
   const isRounded = !isDesktop || rounded;
 
   return (
-    <motion.div
-      animate={{ x: 0 }}
-      whileTap={{ scale: 0.94 }}
-      whileHover={{ scale: 1.02, x: 10 }}
-    >
-      <Button
-        disabled={isDisabled}
-        className="snipcart-add-item"
-        data-item-id={product.id}
-        data-item-price={price}
-        data-item-url={`https://demiselbijoux.com/product/${product.id}`}
-        data-item-description={product.category.title}
-        data-item-image={product.thumbnail.asset.fluid.src}
-        data-item-name={productName}
-        data-item-has-taxes-included={true}
-        isRounded={isRounded}
-      >
-        {!isRounded && (
-          <Box as="span" mr="1rem">
-            Ajouter au panier
-          </Box>
-        )}
-        <BasketIcon />
-      </Button>
-    </motion.div>
+    <StaticQuery
+      query={graphql`
+        query {
+          config: allSanityConfig {
+            edges {
+              node {
+                areDiscountsEnabled
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const areDiscountsEnabled =
+          data.config.edges[0].node.areDiscountsEnabled;
+        const price =
+          (areDiscountsEnabled && getPrice(product, 'discountPrice')) ||
+          getPrice(product, 'salePrice');
+        return (
+          <motion.div
+            animate={{ x: 0 }}
+            whileTap={{ scale: 0.94 }}
+            whileHover={{ scale: 1.02, x: 10 }}
+          >
+            <Button
+              disabled={isDisabled}
+              className="snipcart-add-item"
+              data-item-id={product.id}
+              data-item-price={price}
+              data-item-url={`https://demiselbijoux.com/product/${product.id}`}
+              data-item-description={product.category.title}
+              data-item-image={product.thumbnail.asset.fluid.src}
+              data-item-name={productName}
+              data-item-has-taxes-included={true}
+              isRounded={isRounded}
+            >
+              {!isRounded && (
+                <Box as="span" mr="1rem">
+                  Ajouter au panier
+                </Box>
+              )}
+              <BasketIcon />
+            </Button>
+          </motion.div>
+        );
+      }}
+    />
   );
 };
 
